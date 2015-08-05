ad7a0ffb6d216dccbb40516ac7cd35db963b5574
TheFinestArtist <contact@thefinestartist.com>
2015-08-06 02:01:29 +0900

    Update Serializer

--- app/src/main/java/com/thefinestartist/realm/json/MainActivity.java
+++ app/src/main/java/com/thefinestartist/realm/json/MainActivity.java
@@ -2,17 +2,19 @@ package com.thefinestartist.realm.json;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.ExclusionStrategy;
 import com.google.gson.FieldAttributes;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.orhanobut.logger.Logger;
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
 import com.thefinestartist.realm.json.models.AllTypes;
 import com.thefinestartist.realm.json.models.AllTypesGen;
+import com.thefinestartist.realm.json.serializers.AllTypesSerializer;
+import com.thefinestartist.royal.Rson;
 
 import java.io.StringWriter;
 import java.util.Date;
@@ -53,14 +55,17 @@ public class MainActivity extends AppCompatActivity {
         tryGson(allTypes);
         tryJackson(allTypes);
         tryJPP(allTypes);
+        trySerialize(allTypes);
+        tryRson(allTypes);
     }
 
     private void tryMoshi(AllTypes allTypes) {
         try {
             final Moshi moshi = new Moshi.Builder().build();
             final JsonAdapter<AllTypes> adapter = moshi.adapter(AllTypes.class);
-            Log.d("json", "Moshi(Realm Object): " + adapter.toJson(allTypes));
 
+            Logger.d("Moshi(Realm Object)");
+            Logger.json(adapter.toJson(allTypes));
 
             final AllTypes allTypesProxy;
 
@@ -70,9 +75,10 @@ public class MainActivity extends AppCompatActivity {
             }
             realm.commitTransaction();
 
-            Log.d("json", "Moshi(Proxy Object): " + adapter.toJson(allTypesProxy));
+            Logger.d("Moshi(Proxy Object)");
+            Logger.json(adapter.toJson(allTypesProxy));
         } catch (Exception e) {
-            Log.d("json", "Moshi: error", e);
+            Logger.e("Moshi: error: " + e.getMessage());
         }
     }
 
@@ -91,8 +97,8 @@ public class MainActivity extends AppCompatActivity {
                 })
                 .create();
 
-        Log.d("json", "Gson(Realm Object): " + gson.toJson(allTypes));
-
+        Logger.d("Gson(Realm Object)");
+        Logger.json(gson.toJson(allTypes));
 
         final AllTypes allTypesProxy;
 
@@ -102,7 +108,8 @@ public class MainActivity extends AppCompatActivity {
         }
         realm.commitTransaction();
 
-        Log.d("json", "Gson(Proxy Object): " + gson.toJson(allTypesProxy));
+        Logger.d("Gson(Proxy Object)");
+        Logger.json(gson.toJson(allTypesProxy));
     }
 
     private void tryJPP(AllTypes allTypes) {
@@ -111,33 +118,8 @@ public class MainActivity extends AppCompatActivity {
             AllTypesGen.encode(writer, allTypes);
             writer.close();
 
-            Log.d("json", "JPP(Realm Object): " + writer.toString());
-
-
-            final AllTypes allTypesProxy;
-
-            realm.beginTransaction();
-            {
-                allTypesProxy= realm.copyToRealm(allTypes);
-            }
-            realm.commitTransaction();
-
-            writer = new StringWriter();
-            AllTypesGen.encode(writer, allTypesProxy);
-            writer.close();
-
-            Log.d("json", "JPP(Proxy Object): " + writer.toString());
-        } catch (Exception e) {
-            Log.d("json", "JPP: error", e);
-        }
-    }
-
-    private void tryJackson(AllTypes allTypes) {
-        try {
-            final ObjectMapper mapper = new ObjectMapper();
-
-            Log.d("json", "Jackson(Realm Object): " + mapper.writeValueAsString(allTypes));
-
+            Logger.d("JPP(Realm Object)");
+            Logger.json(writer.toString());
 
             final AllTypes allTypesProxy;
 
@@ -147,9 +129,87 @@ public class MainActivity extends AppCompatActivity {
             }
             realm.commitTransaction();
 
-            Log.d("json", "Jackson(Proxy Object): " + mapper.writeValueAsString(allTypesProxy));
+            writer = new StringWriter();
+            AllTypesGen.encode(writer, allTypesProxy);
+            writer.close();
+
+            Logger.d("JPP(Proxy Object)");
+            Logger.json(writer.toString());
         } catch (Exception e) {
-            Log.d("json", "Jackson: error", e);
+            Logger.e("JPP: error: " + e.getMessage());
         }
+    }
+
+    private void tryJackson(AllTypes allTypes) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+
+            Logger.d("Jackson(Realm Object)");
+            Logger.json(mapper.writeValueAsString(allTypes));
+
+            final AllTypes allTypesProxy;
+
+            realm.beginTransaction();
+            {
+                allTypesProxy = realm.copyToRealm(allTypes);
+            }
+            realm.commitTransaction();
+
+            Logger.d("Jackson(Proxy Object)");
+            Logger.json(mapper.writeValueAsString(allTypesProxy));
+        } catch (Exception e) {
+            Logger.e("Jackson: error: " + e.getMessage());
+        }
+    }
+
+    private void trySerialize(AllTypes allTypes) {
+        try {
+            final Gson gson = new GsonBuilder()
+                    .setExclusionStrategies(new ExclusionStrategy() {
+                        @Override
+                        public boolean shouldSkipField(FieldAttributes f) {
+                            return f.getDeclaringClass().equals(RealmObject.class);
+                        }
+
+                        @Override
+                        public boolean shouldSkipClass(Class<?> clazz) {
+                            return false;
+                        }
+                    })
+                    .registerTypeAdapter(Class.forName("io.realm.AllTypesRealmProxy"), new AllTypesSerializer())
+                    .create();
+
+            Logger.d("Serialize(Realm Object)");
+            Logger.json(gson.toJson(allTypes));
+
+            final AllTypes allTypesProxy;
+
+            realm.beginTransaction();
+            {
+                allTypesProxy = realm.copyToRealm(allTypes);
+            }
+            realm.commitTransaction();
+
+            Logger.d("Serialize(Proxy Object)");
+            Logger.json(gson.toJson(allTypesProxy));
+        } catch (Exception e) {
+            Logger.e("Serialize: error: " + e.getMessage());
+        }
+    }
+
+    private void tryRson(AllTypes allTypes) {
+        Logger.d("Rson(Realm Object)");
+        Logger.json(Rson.toJsonString(allTypes));
+
+        final AllTypes allTypesProxy;
+
+        realm.beginTransaction();
+        {
+            allTypesProxy = realm.copyToRealm(allTypes);
+        }
+        realm.commitTransaction();
+
+        Logger.d("Rson(Proxy Object)");
+        Logger.json(Rson.toJsonString(allTypesProxy));
     }
 }
---
+++ app/src/main/java/com/thefinestartist/realm/json/serializers/AllTypesSerializer.java
@@ -0,0 +1,32 @@
+package com.thefinestartist.realm.json.serializers;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import com.thefinestartist.realm.json.models.AllTypes;
+
+import java.lang.reflect.Type;
+
+/**
+ * Created by TheFinestArtist on 8/6/15.
+ */
+public class AllTypesSerializer implements JsonSerializer<AllTypes> {
+
+    @Override
+    public JsonElement serialize(AllTypes src, Type typeOfSrc, JsonSerializationContext context) {
+        final JsonObject jsonObject = new JsonObject();
+
+        jsonObject.addProperty("columnString", src.getColumnString());
+        jsonObject.addProperty("columnLong", src.getColumnLong());
+        jsonObject.addProperty("columnFloat", src.getColumnFloat());
+        jsonObject.addProperty("columnDouble", src.getColumnDouble());
+        jsonObject.addProperty("columnBoolean", src.isColumnBoolean());
+        jsonObject.addProperty("columnDate", src.getColumnDate().getTime());
+        jsonObject.add("columnBinary", context.serialize(src.getColumnBinary()));
+        jsonObject.add("columnRealmObject", context.serialize(src.getColumnRealmObject()));
+        jsonObject.add("columnRealmList", context.serialize(src.getColumnRealmList()));
+
+        return jsonObject;
+    }
+}
